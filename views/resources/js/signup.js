15c1460444181dd3483d8bfbd4823a3c736946a2
Cory Flynn <coryjamesflynn@hotmail.com>
2020-08-27 12:50:46 -0700

    singup js formatting

--- views/resources/js/signup.js
+++ views/resources/js/signup.js
@@ -9,7 +9,7 @@ function getDriverInfo(id, toggle) {
 }
 
 function submitForm() {
-  var d = document.getElementById("driverYes").value;
+  var d = document.getElementById("dYes").value;
   //var i = parseInt(jQuery("#is18")).val();
   var regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   console.log(d);
