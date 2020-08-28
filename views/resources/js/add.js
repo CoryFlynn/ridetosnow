9911e48637ad60dae5ac52d8a6c58e8600737a58
Cory Flynn <coryjamesflynn@hotmail.com>
2020-08-28 12:11:49 -0700

    add

---
+++ views/resources/js/add.js
@@ -0,0 +1,24 @@
+function submitRide() {
+  var date = $("#dateTime").val();
+  var pay = $("#pay2Input").val();
+  var slots = $("#slotsInput").val();
+  var resort = $("#resort1").val();
+  var description = $("#driverRideDescription").val();
+  var start = $("#start1").val();
+  $.ajax({
+    method: "POST",
+    url: "/setting",
+    data: {
+      user_id: sessionStorage.getItem("user_id"),
+      date: date,
+      pay: pay,
+      slots: slots,
+      resort: resort,
+      description: description,
+      start: start
+    },
+    dataType: "text"
+  }).then((data) => {
+    console.log("html " + sessionStorage.getItem("user_id"));
+  });
+}
